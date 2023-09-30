821c466c16d827d1c4a1c48f12c8af2af0695799
Alexandre Barreto <121456361+IfLostart2per5@users.noreply.github.com>
2023-09-30 20:34:23 -0300

    Update mention.ts

--- src/commands/mention.ts
+++ src/commands/mention.ts
@@ -4,6 +4,3 @@ export const mention = async(socket: BaileysSocket, rJid: string, num: string) =
   await socket.sendMessage(rJid, { text: `@${num}`, mentions: [num + "@s.whatsapp.net"]})
 }
 
-const re: RegExp = /\d+/g;
-const texto: string = "123 456 789"
-console.log(texto.match(re))
