56c36311c59489877c1b61a2f1b8343f80aabc84
Alexandre Barreto <121456361+IfLostart2per5@users.noreply.github.com>
2023-10-09 15:50:02 -0300

    Update mentionAll.ts

--- src/commands/internal/mentionAll.ts
+++ src/commands/internal/mentionAll.ts
@@ -6,12 +6,10 @@ import { BaileysSocket } from '../../types/BaileysSocket';
 export const mentionAll = async(socket: BaileysSocket, arJid: string, grJid: string) => {
   const metadata = await socket.groupMetadata(grJid);
   const participants = metadata.participants.map(x => x.id);
-  /*
   const admins = metadata.participants.filter(x => x.admin).map(x => x.id)
   if (!(admins.includes(arJid))) {
     return;
   }
-  */
   let numbers: string[]= new Array(participants.length);
 
   for (let i = 0; i < participants.length; i++) {
