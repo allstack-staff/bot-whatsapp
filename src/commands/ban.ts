8623504c4c23c8780c2ef6ffc9129ce1517ff60b
GkIgor <igorf0660@gmail.com>
2024-03-27 14:47:08 -0300

    feat: refatorando codigo para melhor instabilidade

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -1,63 +1,63 @@
-import { BaileysSocket } from "../types/BaileysSocket";
-import { MemberList } from "./internal/MemberList";
-import Semaphore from "semaphore-async-await";
 import { readFileSync } from "fs";
 import { resolve } from "path";
+import Semaphore from "semaphore-async-await";
+import { BaileysSocket } from "../types/BaileysSocket";
+import { MemberList } from "./internal/MemberList";
 const regras = JSON.parse(
-  readFileSync(resolve(__dirname, "internal", "regras.json"), "utf-8")
+	readFileSync(resolve(__dirname, "..", "..", "regras.json"), "utf-8"),
 );
 
 function getrule(key: number) {
-  return regras[key];
+	return regras[key];
 }
 export const ban = async (
-  socket: BaileysSocket,
-  members: MemberList,
-  sem: Semaphore,
-  participant: string,
-  groupJid: string,
-  userJid: string,
-  key: any,
-  motivo: string | number
+	socket: BaileysSocket,
+	members: MemberList,
+	sem: Semaphore,
+	participant: string,
+	groupJid: string,
+	userJid: string,
+	key: any,
+	motivo: string | number,
 ) => {
-  await sem.acquire();
+	await sem.acquire();
 
-  if (typeof motivo == "number") {
-    motivo = getrule(motivo) ?? "Regra de Exceção";
-  }
+	if (typeof motivo == "number") {
+		motivo = getrule(motivo) ?? "Regra de Exceção";
+	}
 
-  const metadata = await socket.groupMetadata("120363084400589228@g.us");
-  const admins = metadata.participants.filter((x) => x.id).map((x) => x.id);
+	const metadata = await socket.groupMetadata("120363084400589228@g.us");
+	const admins = metadata.participants.filter((x) => x.id).map((x) => x.id);
 
-  if (admins.includes(participant)) {
-    await socket.sendMessage(groupJid, {
-      react: {
-        text: "❌",
-        key: key,
-      },
-    });
-    return;
-  }
-  if (participant === userJid) {
-    await socket.sendMessage(groupJid, {
-      react: {
-        text: "❌",
-        key: key,
-      },
-    });
-    return;
-  }
-  await socket.sendMessage(groupJid, { react: { text: "✅", key: key } });
-  await socket.groupParticipantsUpdate(groupJid, [userJid], "remove");
-  await socket.sendMessage(groupJid, {
-    text: `O usuário(a) @${userJid?.split(
-      "@"
-    )[0]} foi banido(a). Motivo: ${motivo}`,
-    mentions: [userJid],
-  });
-  members.add(userJid);
-  members.saveMembersList();
-  members.replace(members.name);
+	if (admins.includes(participant)) {
+		await socket.sendMessage(groupJid, {
+			react: {
+				text: "❌",
+				key: key,
+			},
+		});
+		return;
+	}
+	if (participant === userJid) {
+		await socket.sendMessage(groupJid, {
+			react: {
+				text: "❌",
+				key: key,
+			},
+		});
+		return;
+	}
+	await socket.sendMessage(groupJid, { react: { text: "✅", key: key } });
+	await socket.groupParticipantsUpdate(groupJid, [userJid], "remove");
+	await socket.sendMessage(groupJid, {
+		text: `O usuário(a) @${
+			userJid?.split("@")[0]
+		} foi banido(a). Motivo: ${motivo}`,
+		mentions: [userJid],
+	});
+	members.add(userJid);
+	members.saveMembersList();
+	members.replace(members.name);
 
-  sem.release();
+	sem.release();
 };
